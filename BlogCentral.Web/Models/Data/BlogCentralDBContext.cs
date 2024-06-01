af7f669f6a9715f1e2499de7a621167d34c03ffa
iamsourav37 <sourav.g29@outlook.com>
2024-06-01 08:42:40 +0530

    Add project files.

---
+++ BlogCentral.Web/Models/Data/BlogCentralDBContext.cs
@@ -0,0 +1,11 @@
+﻿using BlogCentral.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCentral.Web.Models.Data
+{
+    public class BlogCentralDBContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+    {
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
